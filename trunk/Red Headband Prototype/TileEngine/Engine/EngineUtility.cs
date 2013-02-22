77215414fcb40daf6e8f6b9b96909ec0ae7f06fe
pittmoose@gmail.com <pittmoose@gmail.com@56702703-69eb-7280-86c8-a5a1c8419f03>
2013-02-22 19:46:31 +0000

    Comments

--- trunk/Red Headband Prototype/TileEngine/Engine/EngineUtility.cs
+++ trunk/Red Headband Prototype/TileEngine/Engine/EngineUtility.cs
@@ -1,17 +1,19 @@
-﻿namespace TileEngine.Engine
+﻿// -----------------------------------------------------------------------
+// <copyright file="EngineUtility.cs" company="Me" />
+// Author: Eric S. Policaro
+// -----------------------------------------------------------------------
+namespace TileEngine.Engine
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using Microsoft.Xna.Framework;
     using Microsoft.Xna.Framework.Input;
 
     public class InputUtility
     {
-        public static bool ButtonToggled(GamePadState lastState, GamePadState currentState, Buttons button)
+        public static bool ButtonToggled(GamePadState last, 
+                                         GamePadState current,
+                                         Buttons button)
         {
-            return (currentState.IsButtonDown(button) && lastState.IsButtonUp(button));
+            return (current.IsButtonDown(button) && last.IsButtonUp(button));
         } 
     }
 }
