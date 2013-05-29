708491c4e08cffe31e8fe2e3645bd62108b6f011
pittmoose@gmail.com <pittmoose@gmail.com@56702703-69eb-7280-86c8-a5a1c8419f03>
2013-05-29 12:03:34 +0000

    Documentation additions and cleanup.

--- Red Headband Prototype/Red Headband Prototype/Red Headband Prototype/Levels/Logic/ILevelScript.cs
+++ Red Headband Prototype/Red Headband Prototype/Red Headband Prototype/Levels/Logic/ILevelScript.cs
@@ -1,16 +1,26 @@
-﻿namespace Red_Headband_Prototype.Levels.Logic
+﻿// -----------------------------------------------------------------------
+// ILevelScript.cs: Interface providing a command for level scripting
+// Author: Eric S. Policaro
+// -----------------------------------------------------------------------
+namespace Red_Headband_Prototype.Levels.Logic
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using TileEngine.Engine;
     using Red_Headband_Prototype.Core;
 
+    /// <summary>Interface providing a command for level scripting.</summary>
     public interface ILevelScript
     {
+        /// <summary>
+        /// Gets the map being scripted.
+        /// </summary>
+        /// <returns>GameMap</returns>
         GameMap GetGameMap();
 
+        /// <summary>
+        /// Used to script a level's platforms, camera, and sensors.
+        /// </summary>
+        /// <param name="obj">Player object for the map</param>
+        /// <param name="camera">Camera being used</param>
         void Scripting(PlayerObject obj, ref Camera2D camera);
     }
 }
